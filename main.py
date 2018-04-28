d65eddead6c0fd9e358510c6df065d94e22b29b4
geektype <abdullah@softcomnetworks.com>
2018-04-28 18:55:49 +0100

    started game loop
    Signed-off-by: geektype <abdullah@softcomnetworks.com>

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ from pygame.locals import *
 pygame.init()
 dispSurf = pygame.displayK.set_mode((400, 300))
 pygame.display.set_caption('Alien Ship')
+
+while True:
+    for event in pygame.event.get():
