e0f2cb1c779001c7d69ab0f72e39bcb87272736a
geektype <abdullah@softcomnetworks.com>
2018-04-28 19:15:27 +0100

    Added draw screen function
    Signed-off-by: geektype <abdullah@softcomnetworks.com>

--- main.py
+++ main.py
@@ -6,8 +6,15 @@ pygame.init()
 dispSurf = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption(TITLE)
 
+def drawScreen():
+    dispSurf.fill(BLACK)
+    drawAlien()
+    pygame.display.update()
+    return
+
 while True:
     for event in pygame.event.get():
+        drawScreen()
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
