a497105f327de3e3f6b53c0547236b78463ae3f2
geektype <abdullah@softcomnetworks.com>
2018-04-28 18:58:01 +0100

    added the option to close and changed import
    Signed-off-by: geektype <abdullah@softcomnetworks.com>

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import pygame
+import pygame, sys
 from pygame.locals import *
 
 pygame.init()
-dispSurf = pygame.displayK.set_mode((400, 300))
+dispSurf = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('Alien Ship')
 
 while True:
     for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
