8ccaf71e229ae592cddb00745f785bd112de1b38
geektype <abdullah@softcomnetworks.com>
2018-04-28 22:26:12 +0100

    Ship variables added
    Signed-off-by: geektype <abdullah@softcomnetworks.com>

--- settings.py
+++ settings.py
@@ -4,16 +4,21 @@ HEIGHT = 400
 WIDTH = 300
 FPS = 30
 
-#Alien starting pos
-alienX = 200
-alienY = 20
-alienSize = 10
-alienDir = 0
-alienSpeed = 5
-
 #Define Colors
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
+
+#Alien Info
+alienX = 200
+alienY = 20
+alienSize = 10
+alienDir = 0
+alienSpeed = 5
+
+#Player ship info
+shipX = 200
+shipY = 385
+shipSize = 10
